19b7df54c104d4f0d450e7b32c244c5506ece09c
Rohan P. Singh <rohan565singh@gmail.com>
2022-11-09 19:42:22 +0900

    PPO: More efficient, improved logging

--- run_experiment.py
+++ run_experiment.py
@@ -30,11 +30,14 @@ def run_experiment(args):
     # wrapper function for creating parallelized envs
     env_fn = partial(Env)
     if not args.no_mirror:
-        print("Will use SymmetricEnv.")
-        env_fn = partial(SymmetricEnv, env_fn,
-                         mirrored_obs=env_fn().robot.mirrored_obs,
-                         mirrored_act=env_fn().robot.mirrored_acts,
-                         clock_inds=env_fn().robot.clock_inds)
+        try:
+            print("Wrapping in SymmetricEnv.")
+            env_fn = partial(SymmetricEnv, env_fn,
+                             mirrored_obs=env_fn().robot.mirrored_obs,
+                             mirrored_act=env_fn().robot.mirrored_acts,
+                             clock_inds=env_fn().robot.clock_inds)
+        except AttributeError as e:
+            print("Warning! Cannot use SymmetricEnv.", e)
     obs_dim = env_fn().observation_space.shape[0]
     action_dim = env_fn().action_space.shape[0]
 
@@ -47,7 +50,6 @@ def run_experiment(args):
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
 
-    policy = Gaussian_FF_Actor(obs_dim, action_dim, fixed_std=np.exp(args.std_dev), bounded=False)
     if args.continued:
         path_to_actor = ""
         path_to_pkl = ""
@@ -55,18 +57,21 @@ def run_experiment(args):
             path_to_actor = args.continued
         if os.path.isdir(args.continued):
             path_to_actor = os.path.join(args.continued, "actor.pt")
+        path_to_critic = path_to_actor.split('actor')[0]+'critic'+path_to_actor.split('actor')[1]
         policy = torch.load(path_to_actor)
-    critic = FF_V(obs_dim)
-
-    with torch.no_grad():
-        policy.obs_mean, policy.obs_std = map(torch.Tensor,
-                                              get_normalization_params(iter=args.input_norm_steps,
-                                                                       noise_std=1,
-                                                                       policy=policy,
-                                                                       env_fn=env_fn,
-                                                                       procs=args.num_procs))
-    critic.obs_mean = policy.obs_mean
-    critic.obs_std = policy.obs_std
+        critic = torch.load(path_to_critic)
+    else:
+        policy = Gaussian_FF_Actor(obs_dim, action_dim, fixed_std=np.exp(args.std_dev), bounded=False)
+        critic = FF_V(obs_dim)
+        with torch.no_grad():
+            policy.obs_mean, policy.obs_std = map(torch.Tensor,
+                                                  get_normalization_params(iter=args.input_norm_steps,
+                                                                           noise_std=1,
+                                                                           policy=policy,
+                                                                           env_fn=env_fn,
+                                                                           procs=args.num_procs))
+        critic.obs_mean = policy.obs_mean
+        critic.obs_std = policy.obs_std
     
     policy.train()
     critic.train()
@@ -103,7 +108,6 @@ if __name__ == "__main__":
     parser.add_argument("--clip", type=float, default=0.2, help="Clipping parameter for PPO surrogate loss")
     parser.add_argument("--minibatch_size", type=int, default=64, help="Batch size for PPO updates")
     parser.add_argument("--epochs", type=int, default=3, help="Number of optimization epochs per PPO update") #Xie
-    parser.add_argument("--num_steps", type=int, default=5096, help="Number of sampled timesteps per gradient estimate")
     parser.add_argument("--use_gae", type=bool, default=True,help="Whether or not to calculate returns using Generalized Advantage Estimation")
     parser.add_argument("--num_procs", type=int, default=12, help="Number of threads to train on")
     parser.add_argument("--max_grad_norm", type=float, default=0.05, help="Value to clip gradients at.")
