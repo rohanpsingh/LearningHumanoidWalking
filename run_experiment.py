4818f3079b3338cf8695ccca978acd86d29c674b
Rohan P. Singh <rohan565singh@gmail.com>
2025-01-24 18:36:06 +0900

    run_experiment.py: add eval functionality

--- run_experiment.py
+++ run_experiment.py
@@ -13,6 +13,7 @@ from rl.policies.actor import Gaussian_FF_Actor
 from rl.policies.critic import FF_V
 from rl.envs.normalize import get_normalization_params
 from rl.envs.wrappers import SymmetricEnv
+from rl.utils.eval import EvaluateEnv
 
 def import_env(env_name_str):
     if env_name_str=='jvrc_walk':
@@ -89,32 +90,74 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    if sys.argv[1] != 'train':
-        raise Exception("Invalid usage.")
+    if sys.argv[1] == 'train':
+        sys.argv.remove(sys.argv[1])
 
-    sys.argv.remove(sys.argv[1])
-    parser.add_argument("--env", required=True, type=str)
-    parser.add_argument("--seed", default=0, type=int)
-    parser.add_argument("--logdir", default=Path("/tmp/logs"), type=Path, help="Path to save weights and logs")
-    parser.add_argument("--input-norm-steps", type=int, default=100000)
-    parser.add_argument("--n-itr", type=int, default=20000, help="Number of iterations of the learning algorithm")
-    parser.add_argument("--lr", type=float, default=1e-4, help="Adam learning rate")
-    parser.add_argument("--eps", type=float, default=1e-5, help="Adam epsilon (for numerical stability)")
-    parser.add_argument("--lam", type=float, default=0.95, help="Generalized advantage estimate discount")
-    parser.add_argument("--gamma", type=float, default=0.99, help="MDP discount")
-    parser.add_argument("--std-dev", type=float, default=0.223, help="Action noise for exploration")
-    parser.add_argument("--entropy-coeff", type=float, default=0.0, help="Coefficient for entropy regularization")
-    parser.add_argument("--clip", type=float, default=0.2, help="Clipping parameter for PPO surrogate loss")
-    parser.add_argument("--minibatch-size", type=int, default=64, help="Batch size for PPO updates")
-    parser.add_argument("--epochs", type=int, default=3, help="Number of optimization epochs per PPO update")
-    parser.add_argument("--use-gae", type=bool, default=True,help="Whether or not to calculate returns using Generalized Advantage Estimation")
-    parser.add_argument("--num-procs", type=int, default=12, help="Number of threads to train on")
-    parser.add_argument("--max-grad-norm", type=float, default=0.05, help="Value to clip gradients at")
-    parser.add_argument("--max-traj-len", type=int, default=400, help="Max episode horizon")
-    parser.add_argument("--no-mirror", required=False, action="store_true", help="to use SymmetricEnv")
-    parser.add_argument("--mirror-coeff", required=False, default=0.4, type=float, help="weight for mirror loss")
-    parser.add_argument("--eval-freq", required=False, default=100, type=int, help="Frequency of performing evaluation")
-    parser.add_argument("--continued", required=False, type=Path, help="path to pretrained weights")
-    args = parser.parse_args()
+        sys.argv.remove(sys.argv[1])
+        parser.add_argument("--env", required=True, type=str)
+        parser.add_argument("--logdir", default=Path("/tmp/logs"), type=Path, help="Path to save weights and logs")
+        parser.add_argument("--input-norm-steps", type=int, default=100000)
+        parser.add_argument("--n-itr", type=int, default=20000, help="Number of iterations of the learning algorithm")
+        parser.add_argument("--lr", type=float, default=1e-4, help="Adam learning rate")
+        parser.add_argument("--eps", type=float, default=1e-5, help="Adam epsilon (for numerical stability)")
+        parser.add_argument("--lam", type=float, default=0.95, help="Generalized advantage estimate discount")
+        parser.add_argument("--gamma", type=float, default=0.99, help="MDP discount")
+        parser.add_argument("--std-dev", type=float, default=0.223, help="Action noise for exploration")
+        parser.add_argument("--learn-std", action="store_true", help="Exploration noise will be learned")
+        parser.add_argument("--entropy-coeff", type=float, default=0.0, help="Coefficient for entropy regularization")
+        parser.add_argument("--clip", type=float, default=0.2, help="Clipping parameter for PPO surrogate loss")
+        parser.add_argument("--minibatch-size", type=int, default=64, help="Batch size for PPO updates")
+        parser.add_argument("--epochs", type=int, default=3, help="Number of optimization epochs per PPO update")
+        parser.add_argument("--use-gae", type=bool, default=True,help="Whether or not to calculate returns using Generalized Advantage Estimation")
+        parser.add_argument("--num-procs", type=int, default=12, help="Number of threads to train on")
+        parser.add_argument("--max-grad-norm", type=float, default=0.05, help="Value to clip gradients at")
+        parser.add_argument("--max-traj-len", type=int, default=400, help="Max episode horizon")
+        parser.add_argument("--no-mirror", required=False, action="store_true", help="to use SymmetricEnv")
+        parser.add_argument("--mirror-coeff", required=False, default=0.4, type=float, help="weight for mirror loss")
+        parser.add_argument("--eval-freq", required=False, default=100, type=int, help="Frequency of performing evaluation")
+        parser.add_argument("--continued", required=False, type=Path, help="path to pretrained weights")
+        args = parser.parse_args()
 
-    run_experiment(args)
+        run_experiment(args)
+
+    elif sys.argv[1] == 'eval':
+        sys.argv.remove(sys.argv[1])
+
+        parser.add_argument("--path", required=False, type=Path, default=Path("/tmp/logs"),
+                            help="Path to trained model dir")
+        parser.add_argument("--out-dir", required=False, type=Path, default=None,
+                            help="Path to directory to save videos")
+        parser.add_argument("--ep-len", required=False, type=int, default=10,
+                            help="Episode length to play (in seconds)")
+        args = parser.parse_args()
+
+        path_to_actor = ""
+        if args.path.is_file() and args.path.suffix==".pt":
+            path_to_actor = args.path
+        elif args.path.is_dir():
+            path_to_actor = Path(args.path, "actor.pt")
+        else:
+            raise Exception("Invalid path to actor module: ", args.path)
+
+        path_to_critic = Path(path_to_actor.parent, "critic" + str(path_to_actor).split('actor')[1])
+        path_to_pkl = Path(path_to_actor.parent, "experiment.pkl")
+
+        # load experiment args
+        run_args = pickle.load(open(path_to_pkl, "rb"))
+        # load trained policy
+        policy = torch.load(path_to_actor, weights_only=False)
+        critic = torch.load(path_to_critic, weights_only=False)
+        policy.eval()
+        critic.eval()
+
+        # load experiment args
+        run_args = pickle.load(open(path_to_pkl, "rb"))
+
+        # import the correct environment
+        Env = import_env(run_args.env)
+        yaml_path = Path(run_args.yaml)
+        env = partial(Env, yaml_path)()
+
+        # run
+        e = EvaluateEnv(env, policy, args)
+        e.run()
