278b1de74e0f863dd026e3e3e4752451500faa80
Rohan P. Singh <rohan565singh@gmail.com>
2025-02-18 14:03:36 +0900

    update run_experiment.py

--- run_experiment.py
+++ run_experiment.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 import sys
 import argparse
 import ray
@@ -7,10 +7,9 @@ from functools import partial
 import numpy as np
 import torch
 import pickle
+import shutil
 
 from rl.algos.ppo import PPO
-from rl.policies.actor import Gaussian_FF_Actor
-from rl.policies.critic import FF_V
 from rl.envs.normalize import get_normalization_params
 from rl.envs.wrappers import SymmetricEnv
 from rl.utils.eval import EvaluateEnv
@@ -29,62 +28,36 @@ def run_experiment(args):
     Env = import_env(args.env)
 
     # wrapper function for creating parallelized envs
-    env_fn = partial(Env)
+    env_fn = partial(Env, path_to_yaml=args.yaml)
+    _env = env_fn()
     if not args.no_mirror:
         try:
             print("Wrapping in SymmetricEnv.")
             env_fn = partial(SymmetricEnv, env_fn,
-                             mirrored_obs=env_fn().robot.mirrored_obs,
-                             mirrored_act=env_fn().robot.mirrored_acts,
-                             clock_inds=env_fn().robot.clock_inds)
+                             mirrored_obs=_env.robot.mirrored_obs,
+                             mirrored_act=_env.robot.mirrored_acts,
+                             clock_inds=_env.robot.clock_inds)
         except AttributeError as e:
             print("Warning! Cannot use SymmetricEnv.", e)
-    obs_dim = env_fn().observation_space.shape[0]
-    action_dim = env_fn().action_space.shape[0]
 
     # Set up Parallelism
-    os.environ['OMP_NUM_THREADS'] = '1'
+    #os.environ['OMP_NUM_THREADS'] = '1'  # [TODO: Is this needed?]
     if not ray.is_initialized():
         ray.init(num_cpus=args.num_procs)
 
-    # Set seeds
-    torch.manual_seed(args.seed)
-    np.random.seed(args.seed)
-
-    if args.continued:
-        path_to_actor = ""
-        path_to_pkl = ""
-        if os.path.isfile(args.continued) and args.continued.endswith(".pt"):
-            path_to_actor = args.continued
-        if os.path.isdir(args.continued):
-            path_to_actor = os.path.join(args.continued, "actor.pt")
-        path_to_critic = path_to_actor.split('actor')[0]+'critic'+path_to_actor.split('actor')[1]
-        policy = torch.load(path_to_actor)
-        critic = torch.load(path_to_critic)
-    else:
-        policy = Gaussian_FF_Actor(obs_dim, action_dim, fixed_std=np.exp(args.std_dev), bounded=False)
-        critic = FF_V(obs_dim)
-        with torch.no_grad():
-            policy.obs_mean, policy.obs_std = map(torch.Tensor,
-                                                  get_normalization_params(iter=args.input_norm_steps,
-                                                                           noise_std=1,
-                                                                           policy=policy,
-                                                                           env_fn=env_fn,
-                                                                           procs=args.num_procs))
-        critic.obs_mean = policy.obs_mean
-        critic.obs_std = policy.obs_std
-    
-    policy.train()
-    critic.train()
-
     # dump hyperparameters
-    os.makedirs(args.logdir, exist_ok=True)
-    pkl_path = os.path.join(args.logdir, "experiment.pkl")
+    Path.mkdir(args.logdir, parents=True, exist_ok=True)
+    pkl_path = Path(args.logdir, "experiment.pkl")
     with open(pkl_path, 'wb') as f:
         pickle.dump(args, f)
 
-    algo = PPO(args=vars(args), save_path=args.logdir)
-    algo.train(env_fn, policy, critic, args.n_itr, anneal_rate=args.anneal)
+    # copy config file
+    if args.yaml:
+        config_out_path = Path(args.logdir, "config.yaml")
+        shutil.copyfile(args.yaml, config_out_path)
+
+    algo = PPO(env_fn, args)
+    algo.train(env_fn, args.n_itr)
 
 if __name__ == "__main__":
 
@@ -93,12 +66,11 @@ if __name__ == "__main__":
     if sys.argv[1] == 'train':
         sys.argv.remove(sys.argv[1])
 
-        sys.argv.remove(sys.argv[1])
         parser.add_argument("--env", required=True, type=str)
         parser.add_argument("--logdir", default=Path("/tmp/logs"), type=Path, help="Path to save weights and logs")
         parser.add_argument("--input-norm-steps", type=int, default=100000)
         parser.add_argument("--n-itr", type=int, default=20000, help="Number of iterations of the learning algorithm")
-        parser.add_argument("--lr", type=float, default=1e-4, help="Adam learning rate")
+        parser.add_argument("--lr", type=float, default=1e-4, help="Adam learning rate") # Xie
         parser.add_argument("--eps", type=float, default=1e-5, help="Adam epsilon (for numerical stability)")
         parser.add_argument("--lam", type=float, default=0.95, help="Generalized advantage estimate discount")
         parser.add_argument("--gamma", type=float, default=0.99, help="MDP discount")
@@ -107,7 +79,7 @@ if __name__ == "__main__":
         parser.add_argument("--entropy-coeff", type=float, default=0.0, help="Coefficient for entropy regularization")
         parser.add_argument("--clip", type=float, default=0.2, help="Clipping parameter for PPO surrogate loss")
         parser.add_argument("--minibatch-size", type=int, default=64, help="Batch size for PPO updates")
-        parser.add_argument("--epochs", type=int, default=3, help="Number of optimization epochs per PPO update")
+        parser.add_argument("--epochs", type=int, default=3, help="Number of optimization epochs per PPO update") #Xie
         parser.add_argument("--use-gae", type=bool, default=True,help="Whether or not to calculate returns using Generalized Advantage Estimation")
         parser.add_argument("--num-procs", type=int, default=12, help="Number of threads to train on")
         parser.add_argument("--max-grad-norm", type=float, default=0.05, help="Value to clip gradients at")
@@ -116,6 +88,10 @@ if __name__ == "__main__":
         parser.add_argument("--mirror-coeff", required=False, default=0.4, type=float, help="weight for mirror loss")
         parser.add_argument("--eval-freq", required=False, default=100, type=int, help="Frequency of performing evaluation")
         parser.add_argument("--continued", required=False, type=Path, help="path to pretrained weights")
+        parser.add_argument("--recurrent", required=False, action="store_true", help="use LSTM instead of FF")
+        parser.add_argument("--imitate", required=False, type=str, default=None, help="Policy to imitate")
+        parser.add_argument("--imitate-coeff", required=False, type=float, default=0.3, help="Coefficient for imitation loss")
+        parser.add_argument("--yaml", required=False, type=str, default=None, help="Path to config file passed to Env class")
         args = parser.parse_args()
 
         run_experiment(args)
@@ -155,7 +131,10 @@ if __name__ == "__main__":
 
         # import the correct environment
         Env = import_env(run_args.env)
-        yaml_path = Path(run_args.yaml)
+        if run_args.yaml:
+            yaml_path = Path(run_args.yaml)
+        else:
+            yaml_path = None
         env = partial(Env, yaml_path)()
 
         # run
